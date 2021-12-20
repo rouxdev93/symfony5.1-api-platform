ba5500ca74ea1a0916ac3f2b10ac08c7736894e5
Olatz <rouxdev93@gmail.com>
2021-12-20 20:33:58 +0100

    Creating and configuring the first message with RabbitMQ.

--- api/src/Service/User/UserRegisterService.php
+++ api/src/Service/User/UserRegisterService.php
@@ -6,22 +6,29 @@ namespace App\Service\User;
 
 use App\Entity\User;
 use App\Exception\User\UserAlreadyExistException;
+use App\Messenger\Message\UserRegisteredMessage;
+use App\Messenger\RoutingKey;
 use App\Repository\UserRepository;
 use App\Service\Password\EncoderService;
 use App\Service\Request\RequestService;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class UserRegisterService
 {
     private EncoderService $encoderService;
     private UserRepository $userRepository;
+    private MessageBusInterface $messageBus;
 
     public function __construct(
         EncoderService $encoderService,
-        UserRepository $userRepository
+        UserRepository $userRepository,
+        MessageBusInterface $messageBus
     ){
         $this->userRepository = $userRepository;
         $this->encoderService = $encoderService;
+        $this->messageBus = $messageBus;
     }
 
     public function create(Request $request): User
@@ -39,6 +46,10 @@ class UserRegisterService
 
         $this->userRepository->saveEntity($user);
 
+        $this->messageBus->dispatch(
+            new UserRegisteredMessage($user->getId(), $user->getName(), $user->getEmail(), $user->getToken()),
+            [new AmqpStamp(RoutingKey::USER_QUEUE)]
+        );
         return $user;
     }
 }
