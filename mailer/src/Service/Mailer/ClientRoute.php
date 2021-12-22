639aabbc8a93d3b33dcd1706d42f49836a193e0a
Olatz <rouxdev93@gmail.com>
2021-12-22 10:45:25 +0100

    Consuming first RabbitMq message on user registration.

---
+++ mailer/src/Service/Mailer/ClientRoute.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Mailer\Service\Mailer;
+
+
+class ClientRoute
+{
+    public const ACTIVATE_ACCOUNT = '/activate_account';
+
+
+}
---
+++ mailer/src/Templating/TwigTemplate.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Mailer\Service\Templating;
+
+abstract class TwigTemplate
+{
+    public const USER_REGISTER = 'user/register.twig';
+
+}
